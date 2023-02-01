ba403ff74f3e103466ed7fba636ae19960c7fcef
Pradyumna Paranjape <pradyparanjpe@rediffmail.com>
2023-02-01 11:07:30 +0530

    extend dired configuration
    TODO: column colors

--- modules/pspmacs-editing-enhancement.el
+++ modules/pspmacs-editing-enhancement.el
@@ -64,9 +64,9 @@
 (use-package all-the-icons-dired
   :if (display-graphic-p)
   :hook
-  (dired-mode . (lambda () (interactive)
-                        (unless (file-remote-p default-directory)
-                          (all-the-icons-dired-mode)))))
+  (dired-mode-map . (lambda () (interactive)
+                      (unless (file-remote-p default-directory)
+                        (all-the-icons-dired-mode)))))
 
 (use-package multiple-cursors
   :after evil
