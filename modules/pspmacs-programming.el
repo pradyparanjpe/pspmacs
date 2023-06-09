d4a91df9003db717f887c2ecac78c65d72076cab
Pradyumna Paranjape <pradyparanjpe@rediffmail.com>
2023-06-09 08:00:19 +0530

    Add typescript (javascript) language
    Add tree-sitter to programming
    Emacs v>28 should be compiled with tree-sitter anyways.

--- modules/pspmacs-programming.el
+++ modules/pspmacs-programming.el
@@ -156,6 +156,16 @@
   ;; per mode with `ligature-mode'.
   (global-ligature-mode t))
 
+(use-package tree-sitter
+  :ensure t
+  :config
+  (global-tree-sitter-mode)
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
+
+(use-package tree-sitter-langs
+  :ensure t
+  :after tree-sitter)
+
 (use-package eglot
   :init
   (add-hook 'orderless-style-dispatchers
