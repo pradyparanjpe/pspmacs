pradyparanjpe/pspmacs

commit 39f596b1e4898857735c56ca0cd71f717ff4be3f

Remove code obsolete in v29
Emacs-29 was released today, 2023-07-30

packaging/bootstrap-builtin.el
@@ -19,8 +19,7 @@ t only if ARCHIVE's time-stamp within last `pspmacs/archives-stale-days'"
          (last-update-time (decode-time (file-attribute-modification-time
                                          (file-attributes archive-path))))
          (delta (make-decoded-time :day pspmacs/archives-stale-days)))
-    (cond ((not (file-readable-p archive-path))
-           nil)
+    (cond ((not (file-readable-p archive-path)) nil)
           ((time-less-p (encode-time
                          (decoded-time-add last-update-time delta))
                         (encode-time today))
@@ -33,35 +32,46 @@ t only if ARCHIVE's time-stamp within last `pspmacs/archives-stale-days'"
   (cl-every #'pspmacs/archive-refreshed-recently-p
             (mapcar #'car package-archives)))
 
-(defun pspmacs/init-vc-use-package ()
-  "Emacs v29 can install packages from version control."
-  (unless (version< emacs-version "29")
+(defun pspmacs/init-use-package ()
+  "Emacs v29 can install packages from version control.
+
+vc-use-package is included in Emacs 30"
+  (eval-and-compile
+    (customize-set-variable 'use-package-compute-statistics t)
+    (customize-set-variable 'use-package-always-defer t)
+    (customize-set-variable 'use-package-expand-minimally t))
+
+  ;; NEXT Drop once released with Gnu/Emacs (29 or 30)
+  (when (version< emacs-version "30")
     (unless (package-installed-p 'vc-use-package)
       (package-vc-install "https://github.com/slotThe/vc-use-package"))
     (require 'vc-use-package)))
 
 (defun pspmacs/init-package-manager ()
   "Initialize `package.el' as the package manager"
-  ;; Additional package archives
+  ;; package should store data locally.
+  (customize-set-variable 'package-user-dir
+                          (expand-file-name "packages" local-emacs-dir))
+  (unless (file-exists-p package-user-dir) (mkdir package-user-dir t))
+
   ;; Paranoia
+  ;; (add-to-list 'package-archives
+  ;;              '("stable" . "https://stable.melpa.org/packages/"))
+  ;; ("stable" . 70)
+
+  ;; Additional package archives
   (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 
   (customize-set-variable 'package-archive-priorities
                           '(("gnu"    . 99)
                             ("nongnu" . 80)
                             ("melpa"  . 0)))
-  ;; (add-to-list 'package-archives '("stable" . "https://stable.melpa.org/packages/"))
-  ;; ("stable" . 70)
 
-  ;; package should store data locally.
-  (customize-set-variable 'package-user-dir
-                          (expand-file-name "packages" local-emacs-dir))
-  (unless (file-exists-p package-user-dir)
-    (mkdir package-user-dir t))
   (package-initialize)
   (unless (pspmacs/archives-refreshed-recently-p)
-    (message "Refreshing package archives")
+    (message "Refreshing package archives...")
     (package-refresh-contents))
-  (pspmacs/init-vc-use-package))
+  (pspmacs/init-use-package))
 
+(pspmacs/load-inherit)
 ;;; bootstrap-builtin.el ends here

packaging/bootstrap.org
@@ -1,0 +1,98 @@
+#+title: pspmacs-bootstrap
+#+PROPERTY: header-args :tangle bootstrap-builtin.el :mkdirp t :results no :eval never
+#+auto_tangle: t
+
+* Bootstrap package manager
+- We shall use Emacs's built-in =package.el= by default.
+
+* Check archive freshness
+#+begin_src emacs-lisp
+  ;;; bootstrap-builtin.el --- package.el package manager -*- lexical-binding: t; -*-
+
+  ;; package configuration
+  ;; Code:
+  (require 'time-date)
+  (require 'package)
+  (defcustom pspmacs/archives-stale-days 1
+    "Days after which archives become stale."
+    :type 'integer)
+
+  (defun pspmacs/archive-refreshed-recently-p (archive)
+    "If package ARCHIVE was initialized today.
+
+  t only if ARCHIVE's time-stamp within last `pspmacs/archives-stale-days'"
+    (let* ((today (decode-time nil nil t))
+           (archive-path (expand-file-name
+                          (format "archives/%s/archive-contents" archive)
+                          package-user-dir))
+           (last-update-time (decode-time (file-attribute-modification-time
+                                           (file-attributes archive-path))))
+           (delta (make-decoded-time :day pspmacs/archives-stale-days)))
+      (cond ((not (file-readable-p archive-path)) nil)
+            ((time-less-p (encode-time
+                           (decoded-time-add last-update-time delta))
+                          (encode-time today))
+             nil)
+            (t t))))
+
+  (defun pspmacs/archives-refreshed-recently-p ()
+    "All archives have been refreshed today."
+    (interactive)
+    (cl-every #'pspmacs/archive-refreshed-recently-p
+              (mapcar #'car package-archives)))
+#+end_src
+
+* Emacs-30
+- [[https://github.com/slotThe/vc-use-package][vc-use-package]] will get included in Gnu/Emacs soon.
+#+begin_src emacs-lisp
+  (defun pspmacs/init-use-package ()
+    "Emacs v29 can install packages from version control.
+
+  vc-use-package is included in Emacs 30"
+    (eval-and-compile
+      (customize-set-variable 'use-package-compute-statistics t)
+      (customize-set-variable 'use-package-always-defer t)
+      (customize-set-variable 'use-package-expand-minimally t))
+
+    ;; NEXT Drop once released with Gnu/Emacs (29 or 30)
+    (when (version< emacs-version "30")
+      (unless (package-installed-p 'vc-use-package)
+        (package-vc-install "https://github.com/slotThe/vc-use-package"))
+      (require 'vc-use-package)))
+#+end_src
+
+* Call
+#+begin_src emacs-lisp
+  (defun pspmacs/init-package-manager ()
+    "Initialize `package.el' as the package manager"
+    ;; package should store data locally.
+    (customize-set-variable 'package-user-dir
+                            (expand-file-name "packages" local-emacs-dir))
+    (unless (file-exists-p package-user-dir) (mkdir package-user-dir t))
+
+    ;; Paranoia
+    ;; (add-to-list 'package-archives
+    ;;              '("stable" . "https://stable.melpa.org/packages/"))
+    ;; ("stable" . 70)
+
+    ;; Additional package archives
+    (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+
+    (customize-set-variable 'package-archive-priorities
+                            '(("gnu"    . 99)
+                              ("nongnu" . 80)
+                              ("melpa"  . 0)))
+
+    (package-initialize)
+    (unless (pspmacs/archives-refreshed-recently-p)
+      (message "Refreshing package archives...")
+      (package-refresh-contents))
+    (pspmacs/init-use-package))
+#+end_src
+
+* Inherit
+#+begin_src emacs-lisp
+
+  (pspmacs/load-inherit)
+  ;;; bootstrap-builtin.el ends here
+#+end_src

pspackaging/bootstrap.org
@@ -1,176 +1,0 @@
-#+title: pspmacs-bootstrap
-#+PROPERTY: header-args :tangle pspmacs-browse.el :mkdirp t :results no :eval never
-#+auto_tangle: t
-
-* Bootstrap package manager
-- We shall use Emacs's built-in =package.el= by default.
-#+begin_src emacs-lisp :tangle bootstrap-package.el
-  (defcustom pspmacs/package-manager
-    (if (version< emacs-version "29")
-        'straight
-      'builtin)
-    "Packaging system to use."
-    :type '(string :tag "Emacs package manager to use")
-    :options '(builtin straight))
-
-  (defcustom pspmacs/packaging-directory
-    (expand-file-name "pspackaging" user-emacs-directory)
-    "Packaging recipes directory."
-    :type '(string :tag "Directory with modules to bootstrap package manager"))
-
-  (defcustom pspmacs/install-git-clones
-    (if (string= pspmacs/package-manager "straight") t nil)
-    "Install packages, which require git cloning."
-    :type 'boolean)
-
-  (defun pspmacs/package-bootstrap (&optional manager)
-    "Bootstrap package manager to install and configure emacs packages.
-
-  If MANAGER is a member of straight, cloned, straight.el,
-  `straight.el' package manager is bootstrapped.
-  Else, `pspmacs/package-manager', which defaults to the builtin
-  manager `package.el', is bootstrapped.
-
-  This is the first function called by `init.el'."
-    (let* ((manager
-            (if (member manager '(straight cloned straight.el))
-                'straight
-              pspmacs/package-manager))
-           (manager-file (expand-file-name
-                          (format "bootstrap-%s.el" manager)
-                          pspmacs/packaging-directory)))
-      (if (file-exists-p manager-file)
-          (load manager-file nil 'nomessage)
-        (error "Could not find bootstrap instructions for %s." manager))))
-#+end_src
-
-* Straight.el
-- =straight-base-dir= should be in local tree (=local-emacs-dir=)
-- use gnu-elpa mirror
-- NEXT *DEPRECATION* in version 29, =vc-package-install= does exactly *this*.
-  So, this may be dropped in favour of [[https://github.com/slotThe/vc-use-package][vc-use-package]].
-#+begin_src emacs-lisp :tangle bootstrap-straight.el
-  ;;; bootstrap-straight.el --- straight.el package manager -*- lexical-binding: t; -*-
-  ;;; straight as package manager
-  (defun pspmacs/init-package-manager ()
-    "Initialize `package.el' as the package manager"
-    (setq straight-use-package-by-default t)
-    (setq straight-recipes-gnu-elpa-use-mirror t)
-    (defvar bootstrap-version)
-    (let ((bootstrap-file
-           (expand-file-name
-            "straight/repos/straight.el/bootstrap.el"
-            local-emacs-dir))
-          (bootstrap-version 6))
-      ;; moves the straight install directory to the local machine configuration
-      (setq straight-base-dir local-emacs-dir
-            straight-use-package-by-default t)
-      (unless (file-exists-p bootstrap-file)
-        (message "Bootstrap file not found. Downloading...")
-        (with-current-buffer
-            (url-retrieve-synchronously
-             "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-             'silent 'inhibit-cookies)
-          (goto-char (point-max))
-          (eval-print-last-sexp)))
-      (load bootstrap-file nil 'nomessage)))
-  ;; bootstrap-straight.el ends here
-#+end_src
-
-* Bootstrap builtin package manager
-** Check archive freshness
-#+begin_src emacs-lisp :tangle bootstrap-builtin.el
-  ;;; bootstrap-builtin.el --- package.el package manager -*- lexical-binding: t; -*-
-
-  ;; package configuration
-  ;; Code:
-  (require 'time-date)
-  (require 'package)
-  (defcustom pspmacs/archives-stale-days 1
-    "Days after which archives become stale."
-    :type 'integer)
-
-  (defun pspmacs/archive-refreshed-recently-p (archive)
-    "If package ARCHIVE was initialized today.
-
-  t only if ARCHIVE's time-stamp within last `pspmacs/archives-stale-days'"
-    (let* ((today (decode-time nil nil t))
-           (archive-path (expand-file-name
-                          (format "archives/%s/archive-contents" archive)
-                          package-user-dir))
-           (last-update-time (decode-time (file-attribute-modification-time
-                                           (file-attributes archive-path))))
-           (delta (make-decoded-time :day pspmacs/archives-stale-days)))
-      (cond ((not (file-readable-p archive-path))
-             nil)
-            ((time-less-p (encode-time
-                           (decoded-time-add last-update-time delta))
-                          (encode-time today))
-             nil)
-            (t t))))
-
-  (defun pspmacs/archives-refreshed-recently-p ()
-    "All archives have been refreshed today."
-    (interactive)
-    (cl-every #'pspmacs/archive-refreshed-recently-p
-              (mapcar #'car package-archives)))
-#+end_src
-
-** Emacs-29
-#+begin_src emacs-lisp :tangle bootstrap-builtin.el
-  (defun pspmacs/init-vc-use-package ()
-    "Emacs v29 can install packages from version control."
-    (unless (version< emacs-version "29")
-      (unless (package-installed-p 'vc-use-package)
-        (package-vc-install "https://github.com/slotThe/vc-use-package"))
-      (require 'vc-use-package)))
-#+end_src
-
-** Call
-#+begin_src emacs-lisp :tangle bootstrap-builtin.el
-  (defun pspmacs/init-package-manager ()
-    "Initialize `package.el' as the package manager"
-    ;; Additional package archives
-    ;; Paranoia
-    (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-
-    (customize-set-variable 'package-archive-priorities
-                            '(("gnu"    . 99)
-                              ("nongnu" . 80)
-                              ("melpa"  . 0)))
-    ;; (add-to-list 'package-archives '("stable" . "https://stable.melpa.org/packages/"))
-    ;; ("stable" . 70)
-
-    ;; package should store data locally.
-    (customize-set-variable 'package-user-dir
-                            (expand-file-name "packages" local-emacs-dir))
-    (unless (file-exists-p package-user-dir)
-      (mkdir package-user-dir t))
-    (package-initialize)
-    (unless (pspmacs/archives-refreshed-recently-p)
-      (message "Refreshing package archives")
-      (package-refresh-contents))
-    (pspmacs/init-vc-use-package))
-
-  ;;; bootstrap-builtin.el ends here
-#+end_src
-
-* Use-package
-- Configure use-package system
-- use =use-package=
-- NEXT in version 29, =use-package= is a part of standard Emacs release.
-#+begin_src  emacs-lisp :tangle bootstrap-use-package.el
-  (defun pspmacs/init-use-package ()
-    "Initialize `use-package'"
-    (if (string= pspmacs/package-manager "straight")
-        (straight-use-package 'use-package)
-      (progn
-        (unless (package-installed-p 'use-package)
-          (package-install 'use-package))
-        (setq use-package-always-ensure t)))
-    (require 'use-package)
-    (eval-and-compile
-      (setq use-package-compute-statistics t)
-      (setq use-package-always-defer t)
-      (setq use-package-expand-minimally t)))
-#+end_src

README.org
@@ -75,7 +75,7 @@ Bare Minimal Dependencies: (the installation script tries to install them.)
 - ripgrep (Better, faster, broader grep)
 - Nerd Fonts (fancy StartPage, Mode-line)
 - Gnu/Emacs (Well, obviously.)
-  With flags (at least):
+  Built with flags (at least):
   - --with-pgtk
   - --with-mailutils
   - --with-gnutls

early/index.org
@@ -189,47 +189,35 @@ Initialize configurations
 ** Ensure a =local-emacs-dir=
 - Make if absent
 #+begin_src emacs-lisp :tangle init.el
-  (unless (file-exists-p local-emacs-dir)
-    (mkdir local-emacs-dir t))
+  (unless (file-exists-p local-emacs-dir) (mkdir local-emacs-dir t))
 #+end_src
 
 ** Native compilation settings
 #+begin_src emacs-lisp :tangle init.el
   (when (featurep 'native-compile)
     ;; Silence compiler warnings as they can be pretty disruptive
-    (setq native-comp-async-report-warnings-errors nil)
+    (customize-set-variable 'native-comp-async-report-warnings-errors nil)
 
     ;; Make native compilation happen asynchronously
-    (setq native-comp-deferred-compilation t)
+    (customize-set-variable 'native-comp-deferred-compilation t)
 
     ;; Set the right directory to store the native compilation cache
     ;; NOTE the method for setting the eln-cache directory
     ;; depends on the emacs version
     (when (fboundp 'startup-redirect-eln-cache)
-      (if (version< emacs-version "29")
-          (add-to-list
-           'native-comp-eln-load-path
-           (convert-standard-filename
-            (expand-file-name "var/eln-cache/" local-emacs-dir)))
-        (startup-redirect-eln-cache
-         (convert-standard-filename
-          (expand-file-name "var/eln-cache/" local-emacs-dir)))))
-    (add-to-list
-     'native-comp-eln-load-path
-     (expand-file-name "eln-cache/" local-emacs-dir)))
+      (startup-redirect-eln-cache
+       (convert-standard-filename
+        (expand-file-name "var/eln-cache/" local-emacs-dir))))
+    (add-to-list 'native-comp-eln-load-path
+                 (expand-file-name "eln-cache/" local-emacs-dir)))
 #+end_src
 
 ** Package management
-- User may overwrite the =pspmacs/packaging-directory= in private or local init-tree to customize packaging system.
-- User may want to set =pspmacs/package-manager= to =straight= to clone and build packages locally.
-  - This allows for better grip because the user can decide to stick to different commits, branches, forks.
-- With /builtin/ (default), if optional git-controlled packages are desired, =pspmacs/install-git-clones= should be set to non-nil. (This feature is for future. Currently, only planned, not supported.) It is =t= by default when =straight= is the package manager.
+- User may overwrite the =pspmacs/packaging-directory= in private or local init-tree to customize packaging system and decide how packages are to be initiated.
 - These values are set in the =early-init=, but take effect as the /first step/ of =init.el=.
 #+begin_src emacs-lisp :tangle init.el
   (setq pspmacs/packaging-directory
-        (expand-file-name "pspackaging" user-emacs-directory))
-  (setq pspmacs/package-manager 'builtin)
-  ;; (setq pspmacs/install-git-clones nil)
+        (expand-file-name "packaging" user-emacs-directory))
 #+end_src
 
 ** Single key-press response to yes-or-no

early/init.el
@@ -6,36 +6,27 @@
 
 (load-theme 'deeper-blue t)
 
-(unless (file-exists-p local-emacs-dir)
-  (mkdir local-emacs-dir t))
+(unless (file-exists-p local-emacs-dir) (mkdir local-emacs-dir t))
 
 (when (featurep 'native-compile)
   ;; Silence compiler warnings as they can be pretty disruptive
-  (setq native-comp-async-report-warnings-errors nil)
+  (customize-set-variable 'native-comp-async-report-warnings-errors nil)
 
   ;; Make native compilation happen asynchronously
-  (setq native-comp-deferred-compilation t)
+  (customize-set-variable 'native-comp-deferred-compilation t)
 
   ;; Set the right directory to store the native compilation cache
   ;; NOTE the method for setting the eln-cache directory
   ;; depends on the emacs version
   (when (fboundp 'startup-redirect-eln-cache)
-    (if (version< emacs-version "29")
-        (add-to-list
-         'native-comp-eln-load-path
-         (convert-standard-filename
-          (expand-file-name "var/eln-cache/" local-emacs-dir)))
-      (startup-redirect-eln-cache
-       (convert-standard-filename
-        (expand-file-name "var/eln-cache/" local-emacs-dir)))))
-  (add-to-list
-   'native-comp-eln-load-path
-   (expand-file-name "eln-cache/" local-emacs-dir)))
+    (startup-redirect-eln-cache
+     (convert-standard-filename
+      (expand-file-name "var/eln-cache/" local-emacs-dir))))
+  (add-to-list 'native-comp-eln-load-path
+               (expand-file-name "eln-cache/" local-emacs-dir)))
 
 (setq pspmacs/packaging-directory
-      (expand-file-name "pspackaging" user-emacs-directory))
-(setq pspmacs/package-manager 'builtin)
-;; (setq pspmacs/install-git-clones nil)
+      (expand-file-name "packaging" user-emacs-directory))
 
 (defalias 'yes-or-no-p 'y-or-n-p)
 

late/index.org
@@ -33,22 +33,17 @@
 ** Package installation directory
 We want packages to be cloned/downloaded locally.
 #+begin_src emacs-lisp :tangle package-management.el
-  (customize-set-variable
-   'package-user-dir
-   (expand-file-name "packages" local-emacs-dir))
+  (customize-set-variable 'package-user-dir
+                          (expand-file-name "packages" local-emacs-dir))
 #+end_src
 
 ** Package system
 - Load the package-system (=straight.el= or =package.el=).
   This file is maintained separately.
 #+begin_src emacs-lisp :tangle  package-management.el
-  (load (expand-file-name "bootstrap-package.el" pspmacs/packaging-directory)
-          nil 'nomessage)
-  (pspmacs/package-bootstrap)
+  (load (expand-file-name "bootstrap-builtin.el" pspmacs/packaging-directory)
+        nil 'nomessage)
   (pspmacs/init-package-manager)
-  (load (expand-file-name "bootstrap-use-package.el" pspmacs/packaging-directory)
-          nil 'nomessage)
-  (pspmacs/init-use-package)
 #+end_src
 
 ** Initiate org mode with latest release
@@ -96,7 +91,7 @@ We want packages to be cloned/downloaded locally.
 - Maintain a pspmacs version variable and caller function.
   #+begin_src emacs-lisp :tangle version.el
     (defconst pspmacs/version
-      "0.0.1"
+      "0.0.2"
       "PSPMACS version")
 
     (defun pspmacs/version ()
@@ -161,26 +156,27 @@ Pack of functions, variables, faces for pspmacs
 
 ** Inherit
 #+begin_src emacs-lisp :tangle definitions.el
-    (pspmacs/load-inherit)
+  (pspmacs/load-inherit)
 #+end_src
 
 * Init
 ** Header
 #+begin_src emacs-lisp :tangle init.el
-;;; late/init.el --- Late init file, loads modules -*- lexical-binding: t; -*-
+  ;;; late/init.el --- Late init file, loads modules -*- lexical-binding: t; -*-
 #+end_src
 
 ** Custom file
 - The file ~custom-file~ is used by the Customization UI to store value-setting forms in a customization file, rather than at the end of the =~/.emacs.d/init.el= file. It defaults to =~/.emacs.d/custom.el=. The file is loaded after =late/config.el= file. Any variable values set till that point (=late/config.el=) will be overridden with the values set with the Customization UI and saved in the custom file.
 #+begin_src emacs-lisp :tangle init.el
   (customize-set-variable 'custom-file
-    (expand-file-name "custom.el" local-emacs-dir))
+                          (expand-file-name "custom.el" local-emacs-dir))
 #+end_src
+
 ** Reset gc-cons
 - To hasten early-init, gc-cons was set to highest possible value in =early/definitions.el=
   - Let's reduce it back to 2MB.
 #+begin_src emacs-lisp :tangle init.el
-  (setq gc-cons-threshold (* 2 1000 1000))
+  (customize-set-variable 'gc-cons-threshold (* 2 1000 1000))
 #+end_src
 
 #+begin_note
@@ -198,14 +194,14 @@ gcmh, which is a package presumed to be better at handling gc shall be loaded in
 
 ** Inherit
 #+begin_src emacs-lisp :tangle init.el
-    (pspmacs/load-inherit)
+  (pspmacs/load-inherit)
 #+end_src
 
 * Config
 - Allow for very late configurations.
 ** Headers
 #+begin_src emacs-lisp :tangle config.el
-;;; late/config.el --- Final configurations -*- lexical-binding: t; -*-
+  ;;; late/config.el --- Final configurations -*- lexical-binding: t; -*-
 #+end_src
 
 ** Set theme
@@ -217,5 +213,5 @@ gcmh, which is a package presumed to be better at handling gc shall be loaded in
 
 ** Inherit
 #+begin_src emacs-lisp :tangle config.el
-    (pspmacs/load-inherit)
+  (pspmacs/load-inherit)
 #+end_src

late/init.el
@@ -1,9 +1,9 @@
 ;;; late/init.el --- Late init file, loads modules -*- lexical-binding: t; -*-
 
 (customize-set-variable 'custom-file
-  (expand-file-name "custom.el" local-emacs-dir))
+                        (expand-file-name "custom.el" local-emacs-dir))
 
-(setq gc-cons-threshold (* 2 1000 1000))
+(customize-set-variable 'gc-cons-threshold (* 2 1000 1000))
 
 (pspmacs/load-modules)
 

commit f0ddeef286a41a6a0c44d5e4f68bce67c7e63235

[FIX] use-package bug introduced by commit 39f596b
use-package-always-ensure unset, set to ``t``

packaging/bootstrap-builtin.el
@@ -38,6 +38,7 @@ t only if ARCHIVE's time-stamp within last `pspmacs/archives-stale-days'"
 vc-use-package is included in Emacs 30"
   (eval-and-compile
     (customize-set-variable 'use-package-compute-statistics t)
+    (customize-set-variable 'use-package-always-ensure t)
     (customize-set-variable 'use-package-always-defer t)
     (customize-set-variable 'use-package-expand-minimally t))
 

packaging/bootstrap.org
@@ -51,6 +51,7 @@
   vc-use-package is included in Emacs 30"
     (eval-and-compile
       (customize-set-variable 'use-package-compute-statistics t)
+      (customize-set-variable 'use-package-always-ensure t)
       (customize-set-variable 'use-package-always-defer t)
       (customize-set-variable 'use-package-expand-minimally t))
 